ccac1d41694ac251ec1a855ea5d370594bc7cb31
odaman68000 <odaman68k.amiga@gmail.com>
2018-08-28 23:05:28 +0900

    ND80AIDS最新化

--- nd80aids/build.bat
+++ nd80aids/build.bat
@@ -1,2 +1,9 @@
+zmac --rel msxbios.mac
+ld80 -o msxbios.bin -O bin zout\msxbios.rel
+zmac --rel msxbibc.mac
+ld80 -o msxbibc.bin -O bin zout\msxbibc.rel
 copy /b /y patched.bin+msxbios.bin nd80aids.bin
+copy /b /y patchbc.bin+msxbibc.bin n8aidsbc.bin
+zmac --rel aidscom.mac
+ld80 -o nd80aids.com -O bin zout\aidscom.rel
 pause
